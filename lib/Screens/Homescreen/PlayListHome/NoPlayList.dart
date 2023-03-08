b3f736b7f63d65bdf65f56b8392dcb1f28aed23c
Mohammed Swafvan <swafvansafu07@gmail.com>
2023-03-08 10:08:44 +0530

    search updated

--- lib/Screens/Homescreen/PlayListHome/NoPlayList.dart
+++ lib/Screens/Homescreen/PlayListHome/NoPlayList.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:musician/DB/Models/Song_model.dart';
 import 'package:musician/Themes/Colors.dart';
 
 import '../../PlayList/PlatListsScreen.dart';
@@ -8,56 +10,65 @@ class NoPlayList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 250,
-      width: double.infinity,
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
-            width: double.infinity,
-            child: Padding(
-              padding: EdgeInsets.only(left: 15),
-              child: Text(
-                'Musician',
-                style: TextStyle(
-                  shadows: [Shadow(offset: Offset(4.0, 4.0), blurRadius: 3.0, color: Colors.white10)],
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 38,
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayListsScreen()));
-              },
-              child: Container(
-                decoration: BoxDecoration(color: componentsColor.withOpacity(0.7), borderRadius: BorderRadius.circular(20)),
-                height: 180,
+    return ValueListenableBuilder(
+      valueListenable: Hive.box<SongsDataBase>('PlayListdb').listenable(),
+      builder: (context, Box<SongsDataBase> value, child) {
+        final playLists = Hive.box<SongsDataBase>('PlayListdb');
+        return SizedBox(
+          height: 250,
+          width: double.infinity,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
                 width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.playlist_add,
-                      size: 55,
-                      color: Colors.deepPurple[100],
+                child: Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: Text(
+                    'Musician',
+                    style: TextStyle(
+                      shadows: [Shadow(offset: Offset(4.0, 4.0), blurRadius: 3.0, color: Colors.white10)],
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 38,
                     ),
-                    Text(
-                      'Create Playlist',
-                      style: TextStyle(fontSize: 20, color: Colors.deepPurple[100]),
-                    )
-                  ],
+                  ),
                 ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 20, right: 20),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const PlayListsScreen()));
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(color: componentsColor.withOpacity(0.6), borderRadius: BorderRadius.circular(20)),
+                    height: 190,
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          playLists.isEmpty ? Icons.playlist_add : Icons.library_music,
+                          size: 55,
+                          color: Colors.deepPurple[100],
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          playLists.isEmpty ? 'Create Playlist' : 'Your Playlists',
+                          style: TextStyle(fontSize: 20, color: Colors.deepPurple[100], fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/Screens/SearchScreen/SearchSreen.dart
+++ lib/Screens/SearchScreen/SearchSreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:musician/Screens/AllMusic/ListTiles.dart';
+import 'package:musician/Screens/SearchScreen/SearchListTile.dart';
 import 'package:musician/Themes/Colors.dart';
 import 'package:musician/Themes/BackDropWidget.dart';
 import 'package:musician/Themes/BackGroundDesign.dart';
@@ -32,83 +33,81 @@ class _SearchScreenState extends State<SearchScreen> {
           const Blue(),
           const Purple(),
           const BackdropWidget(),
-          SizedBox(
-            child: ListView(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 40.0),
-                  child: Column(
+          SafeArea(
+            child: SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
                     children: [
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            icon: const Icon(
-                              Icons.arrow_back_ios_new,
-                              size: 26,
-                              color: Colors.white38,
-                            ),
-                          ),
-                          const Text('Search',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 38,
-                                  shadows: [Shadow(offset: Offset(4.0, 4.0), blurRadius: 3.0, color: Colors.white10)]))
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 18),
-                        child: Row(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.deepPurple[50],
-                                borderRadius:
-                                    const BorderRadius.only(topLeft: Radius.circular(30), bottomLeft: Radius.circular(30)),
-                              ),
-                              height: 50,
-                              width: 45,
-                              child: const Padding(padding: EdgeInsets.only(left: 7.0), child: Icon(Icons.search)),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.deepPurple[50],
-                                  borderRadius:
-                                      const BorderRadius.only(bottomRight: Radius.circular(30), topRight: Radius.circular(30))),
-                              height: 50,
-                              width: 300,
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 5.0),
-                                child: TextField(
-                                  onChanged: (value) => updatedList(value),
-                                  textAlign: TextAlign.start,
-                                  style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                                  decoration: const InputDecoration(
-                                      filled: true,
-                                      fillColor: Colors.transparent,
-                                      border: InputBorder.none,
-                                      hintText: 'Songs',
-                                      hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w500)),
-                                ),
-                              ),
-                            ),
-                          ],
+                      IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 26,
+                          color: Colors.white38,
                         ),
-                      )
+                      ),
+                      const Text('Search',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 38,
+                              shadows: [Shadow(offset: Offset(4.0, 4.0), blurRadius: 3.0, color: Colors.white10)]))
                     ],
                   ),
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ListTileWidget(songModel: foundedSongs),
-                )
-              ],
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[50],
+                            borderRadius: const BorderRadius.only(topLeft: Radius.circular(30), bottomLeft: Radius.circular(30)),
+                          ),
+                          height: 50,
+                          width: 45,
+                          child: const Padding(padding: EdgeInsets.only(left: 7.0), child: Icon(Icons.search)),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.deepPurple[50],
+                              borderRadius:
+                                  const BorderRadius.only(bottomRight: Radius.circular(30), topRight: Radius.circular(30))),
+                          height: 50,
+                          width: 300,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 5.0),
+                            child: TextField(
+                              onChanged: (value) => updatedList(value),
+                              textAlign: TextAlign.start,
+                              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                              decoration: const InputDecoration(
+                                  filled: true,
+                                  fillColor: Colors.transparent,
+                                  border: InputBorder.none,
+                                  hintText: 'Songs',
+                                  hintStyle: TextStyle(color: Colors.black87, fontSize: 18, fontWeight: FontWeight.w500)),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  SearchScreenListTile(songModel: foundedSongs),
+                ],
+              ),
             ),
           ),
         ],
